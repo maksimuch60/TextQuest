b881f5f1d0425005af6b34e9baa465681c56ae4e
Maksim Anishchenko <maksimych60@gmail.com>
2022-05-29 18:48:27 +0300

    added new logic and scene loader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,88 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    #region Variables
+
     [Header("UI Elements")]
     [SerializeField] private TextMeshProUGUI _headerLabel;
     [SerializeField] private TextMeshProUGUI _storyLabel;
     [SerializeField] private TextMeshProUGUI _choicesLabel;
+    [SerializeField] private Button _menuButton;
 
     [Header("Initial Setup")]
     [SerializeField] private Step _startStep;
+
+    [Header("External Components")]
+    [SerializeField] private SceneLoader _sceneLoader;
+    [SerializeField] private string _menuSceneName;
+    [SerializeField] private int _menuSceneIndex;
+
+    private Step _currentStep;
+
+    #endregion
+
+
+    #region Unity lifecycle
+
     private void Start()
     {
-        SetStep(_startStep);
+        _menuButton.onClick.AddListener(MenuButtonClicked);
+        SetCurrentStep(_startStep);
     }
 
-    private void SetStep(Step step)
+    private void Update()
     {
+        int choiceIndex = GetPressedButtonIndex();
+
+        if (!IsIndexValid(choiceIndex))
+            return;
+
+        SetCurrentStep(choiceIndex);
+    }
+
+    #endregion
+
+
+    #region Private methods
+
+    private void MenuButtonClicked()
+    {
+        _sceneLoader.LoadScene(_menuSceneIndex);
+    }
+
+    private bool IsIndexValid(int choiceIndex) =>
+        choiceIndex >= 0;
+
+    private void SetCurrentStep(int choiceIndex)
+    {
+        if (_currentStep.Steps.Length <= choiceIndex)
+            return;
+
+        Step nextStep = _currentStep.Steps[choiceIndex];
+        SetCurrentStep(nextStep);
+    }
+
+    private int GetPressedButtonIndex()
+    {
+        int pressedButtonIndex = NumButtonHelper.GetPressedButtonIndex();
+        return pressedButtonIndex - 1;
+    }
+
+    private void SetCurrentStep(Step step)
+    {
+        if (step == null)
+            return;
+
+        _currentStep = step;
+
         _headerLabel.text = step.HeaderText;
         _storyLabel.text = step.StoryText;
         _choicesLabel.text = step.ChoicesText;
     }
+
+    #endregion
 }
--- Assets/Scripts/Step.cs
+++ Assets/Scripts/Step.cs
@@ -2,13 +2,17 @@
 
 public class Step : MonoBehaviour
 {
+    #region Varialbes
+
     public string HeaderText;
 
     [TextArea(4, 6)]
     public string StoryText;
-    
+
     [TextArea(4, 6)]
     public string ChoicesText;
 
     public Step[] Steps;
+
+    #endregion
 }
